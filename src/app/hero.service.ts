76c99c412b898f19824c63851907b7377fc4d3af
Chloe qiu <chloe.qiu@perficient.com>
2018-11-12 10:49:29 +0800

    delete unused code for HERO

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 import { Observable, of } from 'rxjs';
